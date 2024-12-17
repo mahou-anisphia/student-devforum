142b3c3fe8b74b8a3f740d79d5d8a1b4afb76a44
AnisSamaDaisuki <youraisedmeup2004@gmail.com>
2024-12-17 16:48:15 +0700

    bug: removed post PATH in root

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { postRouter } from "~/server/api/routers/post";
 import { userRouter } from "~/server/api/routers/user";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 import { profileRouter } from "./routers/profile";
@@ -9,7 +8,6 @@ import { profileRouter } from "./routers/profile";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   user: userRouter,
   profile: profileRouter,
 });
